fb7050fb40e7fda7249bdf51d1b492b3e93ff5f0
João Henrique <joao.hfgoncalves@senacsp.edu.br>
2024-08-01 16:17:09 -0300

    Atualizar as cores e botao pra limpar

--- Form6.cs
+++ Form6.cs
@@ -1,25 +1,21 @@
-﻿using System.Windows.Forms;
-namespace CalcArea
+﻿using static System.Net.Mime.MediaTypeNames;
+using System;
+
+namespace CalculadoraAreas
 {
     public partial class Form1 : Form
     {
-        private Random random = new Random();
-
         public Form1()
         {
             InitializeComponent();
         }
 
-        private Color GetRandomColor()
-        {
-            return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));  //usei gpt pra ver isso de colocar cores aleatorias
-        }
+
 
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
-            if (RdbTriangulo.Checked)
+            if (RadioTriangulo.Checked)
             {
-                this.BackColor = GetRandomColor();
                 pictureBox1.Image = Image.FromFile(@"C:\Users\joao.hfgoncalves\Desktop\aa\triangulo-5_xl.jpeg");
                 lblFormula.Text = "b*h/2";
                 lblf1.Text = " lado ";
@@ -28,16 +24,23 @@ namespace CalcArea
                 btnb1.Visible = true;
                 btnb3.Visible = false;
                 lblf3.Text = "";
-                btnb2.Focus();
+                btnb1.Focus();
+                
+                this.BackColor = SystemColors.Control;
+                lblFormula.ForeColor = SystemColors.ControlText;
+                lblf1.ForeColor = SystemColors.ControlText;
+                lblf2.ForeColor = SystemColors.ControlText;
+                lblf3.ForeColor = SystemColors.ControlText;
             }
         }
 
+
+
         private void RadioQuadrado_CheckedChanged(object sender, EventArgs e)
         {
-            if (RdbQuadrado.Checked)
+            if (RadioQuadrado.Checked)
             {
-                this.BackColor = GetRandomColor();
-                pictureBox1.Image = Image.FromFile(@"C:\Users\joao.hfgoncalves\Desktop\aa\quadrado.jfif");
+                pictureBox1.Image = Image.FromFile(@"C:\Users\\joao.hfgoncalves\Desktop\aa\quadrado.jfif");
                 lblFormula.Text = "l²";
                 lblf2.Text = " lado ";
                 btnb2.Visible = true;
@@ -45,15 +48,23 @@ namespace CalcArea
                 lblf3.Text = "";
                 btnb1.Visible = false;
                 btnb3.Visible = false;
+                btnb2.Focus();
+                // Change form color to green
+                this.BackColor = Color.Green;
+                lblFormula.ForeColor = Color.White;
+                lblf1.ForeColor = Color.White;
+                lblf2.ForeColor = Color.White;
+                lblf3.ForeColor = Color.White;
             }
         }
 
+
+
         private void RadioRetangulo_CheckedChanged(object sender, EventArgs e)
         {
-            if (RdbRetangulo.Checked)
+            if (RadioRetangulo.Checked)
             {
-                this.BackColor = GetRandomColor();
-                pictureBox1.Image = Image.FromFile(@"C:\Users\joao.hfgoncalves\Desktop\aa\retangulo-roxo-claro.jpg");
+                pictureBox1.Image = Image.FromFile(@"C:\Users\\joao.hfgoncalves\Desktop\aa\retangulo-roxo-claro.jpg");
                 lblFormula.Text = "b*h";
                 lblf3.Text = "";
                 lblf1.Text = " base ";
@@ -61,15 +72,23 @@ namespace CalcArea
                 btnb2.Visible = true;
                 btnb1.Visible = true;
                 btnb3.Visible = false;
+                btnb1.Focus();
+                // Reset form color
+                this.BackColor = SystemColors.Control;
+                lblFormula.ForeColor = SystemColors.ControlText;
+                lblf1.ForeColor = SystemColors.ControlText;
+                lblf2.ForeColor = SystemColors.ControlText;
+                lblf3.ForeColor = SystemColors.ControlText;
             }
         }
 
+
+
         private void RadioCirculo_CheckedChanged(object sender, EventArgs e)
         {
-            if (RdbCirculo.Checked)
+            if (RadioCirculo.Checked)
             {
-                this.BackColor = GetRandomColor();
-                pictureBox1.Image = Image.FromFile(@"C:\Users\joao.hfgoncalves\Desktop\aa\250px-Circle_-_black_simple.svg.png");
+                pictureBox1.Image = Image.FromFile(@"C:\Users\\joao.hfgoncalves\Desktop\aa\250px-Circle_-_black_simple.svg.png");
                 lblFormula.Text = "πr²";
                 lblf1.Text = "";
                 lblf3.Text = "";
@@ -77,15 +96,23 @@ namespace CalcArea
                 btnb1.Visible = false;
                 btnb2.Visible = true;
                 btnb3.Visible = false;
+                btnb2.Focus();
+               
+                this.BackColor = SystemColors.Control;
+                lblFormula.ForeColor = SystemColors.ControlText;
+                lblf1.ForeColor = SystemColors.ControlText;
+                lblf2.ForeColor = SystemColors.ControlText;
+                lblf3.ForeColor = SystemColors.ControlText;
             }
         }
 
+
+
         private void RadioTrapezio_CheckedChanged(object sender, EventArgs e)
         {
-            if (RdbTrapezio.Checked)
+            if (RadioTrapezio.Checked)
             {
-                this.BackColor = GetRandomColor();
-                pictureBox1.Image = Image.FromFile(@"C:\Users\joao.hfgoncalves\Desktop\aa\download.png");
+                pictureBox1.Image = Image.FromFile(@"C:\Users\\joao.hfgoncalves\Desktop\aa\download.png");
                 lblFormula.Text = "((B+b)*h/2)";
                 lblf1.Text = " base (B)";
                 lblf2.Text = " base (b) ";
@@ -93,18 +120,163 @@ namespace CalcArea
                 btnb2.Visible = true;
                 btnb1.Visible = true;
                 btnb3.Visible = true;
+                btnb1.Focus();
+               
+                this.BackColor = Color.Cyan;
+                lblFormula.ForeColor = Color.Black;
+                lblf1.ForeColor = Color.Black;
+                lblf2.ForeColor = Color.Black;
+                lblf3.ForeColor = Color.Black;
             }
         }
 
+
+
+        private void lblFormula_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+
         private void btnfazer_Click(object sender, EventArgs e)
         {
-            
+            double lado1 = 0;
+            double lado2 = 0;
+            double lado3 = 0;
+            double resultado;
+
+
+
+           
+            if (RadioTrapezio.Checked)
+            {
+                if (double.TryParse(btnb1.Text, out lado1) &&
+                double.TryParse(btnb2.Text, out lado2) &&
+                double.TryParse(btnb3.Text, out lado3))
+                {
+                    resultado = Math.Round((lado1 + lado2) * lado3 / 2, 2);
+                    lblresult.Text = resultado.ToString();
+                    lblexp.Text = $"(({lado1} + {lado2}) * {lado3}) / 2";
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, insira valores numéricos válidos.");
+                }
+            }
+            else if (RadioTriangulo.Checked)
+            {
+                if (double.TryParse(btnb1.Text, out lado1) &&
+                double.TryParse(btnb2.Text, out lado2))
+                {
+                    resultado = Math.Round((lado1 * lado2) / 2, 2);
+                    lblresult.Text = resultado.ToString();
+                    lblexp.Text = $"{lado1} * {lado2} / 2";
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, insira valores numéricos válidos.");
+                }
+            }
+            else if (RadioQuadrado.Checked)
+            {
+                if (double.TryParse(btnb2.Text, out lado1))
+                {
+                    resultado = Math.Round(lado1 * lado1, 2);
+                    lblresult.Text = resultado.ToString();
+                    lblexp.Text = $"{lado1}²";
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, insira valores numéricos válidos.");
+                }
+            }
+            else if (RadioRetangulo.Checked)
+            {
+                if (double.TryParse(btnb1.Text, out lado1) &&
+                double.TryParse(btnb2.Text, out lado2))
+                {
+                    resultado = Math.Round(lado1 * lado2, 2);
+                    lblresult.Text = resultado.ToString();
+                    lblexp.Text = $"{lado1} * {lado2}";
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, insira valores numéricos válidos.");
+                }
+            }
+            else if (RadioCirculo.Checked)
+            {
+                if (double.TryParse(btnb2.Text, out lado2))
+                {
+                    resultado = Math.Round(Math.PI * lado2 * lado2, 2);
+                    lblresult.Text = resultado.ToString();
+                    lblexp.Text = $"π * {lado2}²";
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, insira valores numéricos válidos.");
+                }
+            }
+
+
+
+            
+            btnb1.Text = "";
+            btnb2.Text = "";
+            btnb3.Text = "";
         }
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        //aqui usei gpt pra tirar uma duvidas bobas
+
+        private void btnlimpa_Click(object sender, EventArgs e)
+        {
+            
+            RadioTriangulo.Checked = false;
+            RadioQuadrado.Checked = false;
+            RadioRetangulo.Checked = false;
+            RadioCirculo.Checked = false;
+            RadioTrapezio.Checked = false;
+
+
+
+          
+            btnb1.Text = "";
+            btnb2.Text = "";
+            btnb3.Text = "";
+
+
+
+           
+            lblFormula.Text = "";
+            lblf1.Text = "";
+            lblf2.Text = "";
+            lblf3.Text = "";
+            lblresult.Text = "";
+            lblexp.Text = "";
+
+
+
+           
+            btnb1.Visible = false;
+            btnb2.Visible = false;
+            btnb3.Visible = false;
+
+
+
+            
+            this.BackColor = SystemColors.Control;
+            lblFormula.ForeColor = SystemColors.ControlText;
+            lblf1.ForeColor = SystemColors.ControlText;
+            lblf2.ForeColor = SystemColors.ControlText;
+            lblf3.ForeColor = SystemColors.ControlText;
         }
     }
 }
-
